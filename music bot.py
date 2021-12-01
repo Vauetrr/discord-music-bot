230bf3c3b7f030b1a1530d628a4f14c10a7a73bd
Vauetrr <izag0502@gmail.com>
2021-12-01 19:18:47 +1100

    implemented voice channel connection

--- music bot.py
+++ music bot.py
@@ -1,18 +1,38 @@
 import discord
+from discord.ext import commands
 import os
 
-client = discord.Client()
+client = commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
     print(f'{client.user} logged in')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
     
-    if message.content.startswith('cool'):
-        await message.channel.send('Cool beans')
+#     if message.content.startswith('cool'):
+#         await message.channel.send('Cool beans')
+
+@client.command()
+async def play(ctx, url):
+    if ctx.author.voice:
+        voiceChannel = ctx.author.voice.channel
+        print('first')
+    else:
+        voiceChannel = discord.utils.get(ctx.guild.voice_channels)
+        if not voiceChannel:
+            await ctx.channel.send('There are no voice channels dickhead')
+            return
+
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice is not None:
+        if not voice.is_connected():
+            await voice.disconnect()
+            await voiceChannel.connect()
+    else:
+        await voiceChannel.connect()
 
 client.run(os.getenv('TOKEN'))
