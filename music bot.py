5c6bdc85f7fb3d0b13466618bee6c87146fd7d4a
Vauetrr <izag0502@gmail.com>
2021-12-01 17:49:19 +1100

    implemented login

--- music bot.py
+++ music bot.py
@@ -1,1 +1,18 @@
 import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} logged in')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('cool'):
+        await message.channel.send('Cool beans')
+
+client.run(os.getenv('TOKEN'))
